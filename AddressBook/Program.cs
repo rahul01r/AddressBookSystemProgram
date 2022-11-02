19c744835c05a0b2e03d776a4e0fd1672cacee8c
rahul01r <rrdubeynibm@gmail.com>
2022-11-02 21:58:05 +0530

    [rahul] Add:UC1-ContactDetails

---
+++ AddressBook/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact()
+            {
+                FirstName = "Rahul Ranjan",
+                LastName = "Dubey",
+                Address = "Ranchi",
+                City = "Ranchi",
+                State = "Jharkhand",
+                Zip = 834001,
+                PhoneNumber = 635313213,
+                Email = "rrdubeyni@gmail.com",
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    }
+}
+    
