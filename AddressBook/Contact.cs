8f55eb59b10d50f463b652ec6918cbb28cd2e1bc
rahul01r <rrdubeynibm@gmail.com>
2022-11-03 09:58:33 +0530

    [rahul] Add:UC2-AddToContact

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -8,16 +8,15 @@ namespace AddressBook
 {
     public class Contact
     {
-       
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Address { get; set; }
-            public string City { get; set; }
-            public string State { get; set; }
 
-            public int Zip { get; set; }
-            public int PhoneNumber { get; set; }
-            public string Email { get; set; }
-        
+        public string firstName;
+        public string lastName;
+        public string address;
+        public int phone;
+        public string city;
+        public string state;
+        public int zipcode;
+        public string email;
+
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,22 +1,59 @@
 ﻿namespace AddressBook
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        //creating list of contact
+        public static List<Contact> contactList = new List<Contact>();
+        public static void Main(string[] args)
         {
-            Contact contact = new Contact()
+            Console.WriteLine("Welcome to Address Book Sytem.");
+
+            Console.WriteLine("Do you want to add new contact press 1 or press 2 to cancle.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            while (num == 1)
             {
-                FirstName = "Rahul Ranjan",
-                LastName = "Dubey",
-                Address = "Ranchi",
-                City = "Ranchi",
-                State = "Jharkhand",
-                Zip = 834001,
-                PhoneNumber = 635313213,
-                Email = "rrdubeyni@gmail.com",
-            };
-            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                Contact contact1 = new Contact();
+                Console.WriteLine("Enter first name: ");
+                contact1.firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter last name: ");
+                contact1.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter address: ");
+                contact1.address = Console.ReadLine();
+
+                Console.WriteLine("Enter city: ");
+                contact1.city = Console.ReadLine();
+
+                Console.WriteLine("Enter state: ");
+                contact1.state = Console.ReadLine();
+
+                Console.WriteLine("Enter phone: ");
+                contact1.phone = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter email: ");
+                contact1.email = Console.ReadLine();
+
+                Console.WriteLine("Enter zipcode: ");
+                contact1.zipcode = Convert.ToInt32(Console.ReadLine());
+
+
+                contactList.Add(contact1);
+
+                Console.WriteLine("Do you want to add anoter contact then press 1 or press 2 for exit ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Total number of contact in address book:" + contactList.Count);
+
+            foreach (Contact contact in contactList)
+            {
+                Console.WriteLine(contact.firstName);
+            }
+
         }
     }
+
 }
     
