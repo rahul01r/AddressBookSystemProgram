95e67728ccc0fc389e45aaceb23a6beddb656e5b
rahul01r <rrdubeynibm@gmail.com>
2022-11-03 10:15:19 +0530

    [rahul] Add:UC4-RemoveContact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -55,13 +55,15 @@
                 Console.WriteLine(contact.firstName);
             }
 
-            Console.WriteLine("Do you want to edit contact details then press 1 or pres 2 for continue: ");
+            //deleting contact
+            Console.WriteLine("Do you want to delete contact press 1 to delete or press 2 to cancle.");
             num = Convert.ToInt32(Console.ReadLine());
 
-            while (num == 1)
+            while (num == 1 && contactList.Count > 0)
             {
-                Console.WriteLine("Enter first name to edit details: ");
+                Console.WriteLine("Enter contact First name");
                 string firstName = Console.ReadLine();
+
                 bool found = false;
                 for (int i = 0; i < contactList.Count; i++)
                 {
@@ -70,47 +72,29 @@
                     {
                         found = true;  //found the contact
 
-                        //now editing...
-                        Console.WriteLine("Enter first name: ");
-                        contactList[i].firstName = Console.ReadLine();
-
-                        Console.WriteLine("Enter last name: ");
-                        contactList[i].lastName = Console.ReadLine();
-
-                        Console.WriteLine("Enter address: ");
-                        contactList[i].address = Console.ReadLine();
-
-                        Console.WriteLine("Enter city: ");
-                        contactList[i].city = Console.ReadLine();
-
-                        Console.WriteLine("Enter state: ");
-                        contactList[i].state = Console.ReadLine();
-
-                        Console.WriteLine("Enter phone: ");
-                        contactList[i].phone = Convert.ToInt32(Console.ReadLine());
-
-                        Console.WriteLine("Enter email: ");
-                        contactList[i].email = Console.ReadLine();
-
-                        Console.WriteLine("Enter zipcode: ");
-                        contactList[i].zipcode = Convert.ToInt32(Console.ReadLine());
+                        contactList.RemoveAt(i);
                         break;
 
                     }
-                } 
-                if (!found)
-                    Console.WriteLine("the contact with given person {0} is not in address book", firstName);
-                //print contacts
+                }
+
+                if (found)
+                {
+                    if (contactList.Count == 0) //if size 0 nothing to delete further
+                        break;
+                }
+                else
+                    Console.WriteLine("the contact with given person '{0}' is not in address book", firstName);
+
                 Console.WriteLine("Current contacts in adress book");
                 foreach (Contact contact in contactList)
                 {
                     Console.WriteLine(contact.firstName);
                 }
-                Console.WriteLine("Do you want to edit contact press 1 to edit or press 2 to cancle.");
+                Console.WriteLine("Do you want to delete contact press 1 to delete or press 2 to cancle.");
                 num = Convert.ToInt32(Console.ReadLine());
             }
         }
     }
-
 }
     
